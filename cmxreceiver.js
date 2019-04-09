1a01a01f308974512d61644a475459773e89f8e6
Charles Berman <Charles.Berman@Magna.com>
2019-04-09 10:12:21 -0400

    updated max value for bodyParser

--- cmxreceiver.js
+++ cmxreceiver.js
@@ -13,45 +13,46 @@ A basic web service to accept CMX data from a Cisco Meraki network
 
 // CHANGE THESE CONFIGURATIONS to match your CMX configuration
 var port = process.env.OVERRIDE_PORT || process.env.PORT || 1890;
-var secret = process.env.SECRET || "enterYourSecret";
-var validator = process.env.VALIDATOR || "enterYourValidator";
-var route = process.env.ROUTE || "/cmx";
+var secret = process.env.SECRET || 'enterYourSecret';
+var validator = process.env.VALIDATOR || 'enterYourValidator';
+var route = process.env.ROUTE || '/cmx';
 
 // All CMX JSON data will end up here. Send it to a database or whatever you fancy.
 // data format specifications: https://documentation.meraki.com/MR/Monitoring_and_Reporting/CMX_Analytics#Version_2.0
 function cmxData(data) {
-    console.log("JSON Feed: " + JSON.stringify(data, null, 2));
-};
-
+	console.log('JSON Feed: ' + JSON.stringify(data, null, 2));
+}
 
 //**********************************************************
 
-// Express Server 
+// Express Server
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
-app.use(bodyParser.json())
+var bodyParser = require('body-parser');
+
+//
+app.use(bodyParser.json({ limit: '25mb' }));
 
 // CMX Location Protocol, see https://documentation.meraki.com/MR/Monitoring_and_Reporting/CMX_Analytics#API_Configuration
 //
 // Meraki asks for us to know the secret
-app.get(route, function (req, res) {
-    console.log("Validator = " + validator);
-    res.status(200).send(validator);
+app.get(route, function(req, res) {
+	console.log('Validator = ' + validator);
+	res.status(200).send(validator);
 });
 //
 // Getting the flow of data every 1 to 2 minutes
-app.post(route, function (req, res) {
-    if (req.body.secret == secret) {
-        console.log("Secret verified");
-        cmxData(req.body);
-    } else {
-        console.log("Secret was invalid");
-    }
-    res.status(200);
+app.post(route, function(req, res) {
+	if (req.body.secret == secret) {
+		console.log('Secret verified');
+		cmxData(req.body);
+	} else {
+		console.log('Secret was invalid');
+	}
+	res.status(200);
 });
 
 // Start server
-app.listen(port, function () {
-    console.log("CMX Receiver listening on port: " + port);
+app.listen(port, function() {
+	console.log('CMX Receiver listening on port: ' + port);
 });
