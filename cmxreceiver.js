a2d2ad692136c562625675638ce24c54175b8c7a
Dexter LaBora <cguynn@gmail.com>
2020-09-11 10:30:38 +0200

    updated to reflect new Scanning API docs

--- cmxreceiver.js
+++ cmxreceiver.js
@@ -1,5 +1,7 @@
 /*
-NodeJS CMX Receiver
+NodeJS CMX / Scanning API Receiver
+
+https://developer.cisco.com/meraki/scanning-api/#!overview/overview
 
 A basic web service to accept CMX data from a Cisco Meraki network
 - Accept a GET request from Meraki and respond with a validator
@@ -18,7 +20,7 @@ var validator = process.env.VALIDATOR || 'enterYourValidator';
 var route = process.env.ROUTE || '/cmx';
 
 // All CMX JSON data will end up here. Send it to a database or whatever you fancy.
-// data format specifications: https://documentation.meraki.com/MR/Monitoring_and_Reporting/CMX_Analytics#Version_2.0
+
 function cmxData(data) {
 	console.log('JSON Feed: ' + JSON.stringify(data, null, 2));
 }
@@ -29,19 +31,16 @@ function cmxData(data) {
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
-//
 app.use(bodyParser.json({ limit: '25mb' }));
 
-// CMX Location Protocol, see https://documentation.meraki.com/MR/Monitoring_and_Reporting/CMX_Analytics#API_Configuration
-//
-// Meraki asks for us to know the secret
+
+// Meraki asks for us to know the validator
 app.get(route, function(req, res) {
 	console.log('Validator = ' + validator);
 	res.status(200).send(validator);
 });
-//
-// Getting the flow of data every 1 to 2 minutes
+
+// Getting the flow of data every minute
 app.post(route, function(req, res) {
 	if (req.body.secret == secret) {
 		console.log('Secret verified');
